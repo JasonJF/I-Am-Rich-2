1ce4c9419a59745670bdc9d84f899e6a4d379dff
JasonJF <47773807+JasonJF@users.noreply.github.com>
2021-09-01 14:17:31 +0200

    Initial Commit

---
+++ I Am Rich 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich 2
+//
+//  Created by Jason Folding on 2021/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
